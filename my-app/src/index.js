d7e72f51e1139bf6931f8bf874bc10ebef594050
nero-15 <nero15.dev@gmail.com>
2021-07-08 22:10:25 +0900

    tick

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -158,8 +158,21 @@ const element = (
     Hello, {formatName(user)}!
   </h1>
 );
-ReactDOM.render(
-    element,
-//    <Game />,
-  document.getElementById('root')
-);
+//
+// ReactDOM.render(
+//     element,
+// //    <Game />,
+//   document.getElementById('root')
+// );
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
