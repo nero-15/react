78707b031073e6bb8686a380f67eee70c1d31176
nero-15 <nero15.dev@gmail.com>
2021-07-08 21:18:37 +0900

    fix handleclick

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -50,7 +50,7 @@ class Board extends React.Component {
 
 class Game extends React.Component {
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -61,6 +61,7 @@ class Game extends React.Component {
           history: history.concat([{
             squares: squares,
           }]),
+          stepNumber: history.length,
           xIsNext: !this.state.xIsNext,
         });
     }
@@ -83,7 +84,7 @@ class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
