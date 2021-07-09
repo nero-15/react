359c12abc7bdac760b65793ab99b816ffcfaaf29
nero-15 <nero15.dev@gmail.com>
2021-07-09 22:01:47 +0900

    func compornent prop

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -144,20 +144,6 @@ function calculateWinner(squares) {
 
 // ========================================
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
 //
 // ReactDOM.render(
 //     element,
@@ -165,14 +151,25 @@ const element = (
 //   document.getElementById('root')
 // );
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+const element = <Welcome name="Sara" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+// https://ja.reactjs.org/docs/rendering-elements.html
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+//
+// setInterval(tick, 1000);
