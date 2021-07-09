5d84e481188e2e122efeb39596814b44392f2378
nero-15 <nero15.dev@gmail.com>
2021-07-09 22:09:20 +0900

    avator

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -165,6 +165,35 @@ function App() {
   );
 }
 
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <Avatar user={props.author} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
