997e5ddae6947cf815b6e6259d19fdadd8a8ad67
nero-15 <nero15.dev@gmail.com>
2021-07-10 21:53:49 +0900

    toggle"

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -256,3 +256,34 @@ class Clock extends React.Component {
 //    <Clock />,
 //     document.getElementById('root')
 // );
+
+
+// https://ja.reactjs.org/docs/handling-events.html
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
