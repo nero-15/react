8c483143511256342cf28394ea53ecfb11d6005c
nero-15 <nero15.dev@gmail.com>
2021-07-10 21:28:52 +0900

    func to comp

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -217,13 +217,15 @@ const comment = {
 //   document.getElementById('root')
 // );
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 function tick() {
