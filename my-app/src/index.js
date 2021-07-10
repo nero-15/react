4f37f8b13fd38b77ebed2b3ed2de5391ae26dcf5
nero-15 <nero15.dev@gmail.com>
2021-07-10 22:37:04 +0900

    name form

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -387,3 +387,38 @@ const numbers = [1, 2, 3, 4, 5];
 // );
 
 // https://ja.reactjs.org/docs/forms.html
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('root')
+);
