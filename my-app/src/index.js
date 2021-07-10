8fa58d0834a65468ced0ec88b2e326a15bd54e2a
nero-15 <nero15.dev@gmail.com>
2021-07-10 22:23:51 +0900

    list items

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -368,5 +368,10 @@ class LoginControl extends React.Component {
 
 // https://ja.reactjs.org/docs/lists-and-keys.html
 const numbers = [1, 2, 3, 4, 5];
-const doubled = numbers.map((number) => number * 2);
-console.log(doubled);
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+ReactDOM.render(
+  <ul>{listItems}</ul>,
+  document.getElementById('root')
+);
