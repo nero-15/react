00221683106097d0384bc6a1bddba94f0ab2e437
nero-15 <nero15.dev@gmail.com>
2021-07-10 22:49:48 +0900

    Calculator

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -466,3 +466,40 @@ class FlavorForm extends React.Component {
 // );
 
 // https://ja.reactjs.org/docs/lifting-state-up.html
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+        <BoilingVerdict
+          celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Calculator  />,
+  document.getElementById('root')
+);
