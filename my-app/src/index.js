39b4b997be48a8bfa4ef2783f483a6bdf8504b95
nero-15 <nero15.dev@gmail.com>
2021-07-10 21:26:58 +0900

    clock

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -212,20 +212,25 @@ const comment = {
   },
 };
 
-ReactDOM.render(
-  <Comment author={comment.author} text={comment.text} date={comment.date} />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Comment author={comment.author} text={comment.text} date={comment.date} />,
+//   document.getElementById('root')
+// );
 
-// https://ja.reactjs.org/docs/rendering-elements.html
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   ReactDOM.render(element, document.getElementById('root'));
-// }
-//
-// setInterval(tick, 1000);
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function tick() {
+  ReactDOM.render(
+     <Clock date={new Date()} />,
+      document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
