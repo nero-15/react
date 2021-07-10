f5d2de1e734912253e1f43158911578feab82230
nero-15 <nero15.dev@gmail.com>
2021-07-10 23:04:39 +0900

    dialog

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -564,3 +564,29 @@ class Calculator extends React.Component {
 // );
 
 // https://ja.reactjs.org/docs/composition-vs-inheritance.html
+
+function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
+      {props.children}
+    </div>
+  );
+}
+
+function WelcomeDialog() {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">
+        Welcome
+      </h1>
+      <p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+    </FancyBorder>
+  );
+}
+
+ReactDOM.render(
+  <WelcomeDialog  />,
+  document.getElementById('root')
+);
