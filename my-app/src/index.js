cab9c002e7e85fcfaf203e963efb55bdb7cd3304
nero-15 <nero15.dev@gmail.com>
2021-07-10 22:09:35 +0900

    greeting

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -283,7 +283,30 @@ class Toggle extends React.Component {
   }
 }
 
+// ReactDOM.render(
+//   <Toggle />,
+//   document.getElementById('root')
+// );
+
+// https://ja.reactjs.org/docs/conditional-rendering.html
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
 ReactDOM.render(
-  <Toggle />,
+  // Try changing to isLoggedIn={true}:
+  <Greeting isLoggedIn={false} />,
   document.getElementById('root')
 );
