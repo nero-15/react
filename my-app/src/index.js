0c96b31f0fd7a2f5023fa2c5406f37ac765a29e1
nero-15 <nero15.dev@gmail.com>
2021-07-10 21:31:13 +0900

    const

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -218,19 +218,23 @@ const comment = {
 // );
 
 class Clock extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
 function tick() {
   ReactDOM.render(
-     <Clock date={new Date()} />,
+     <Clock />,
       document.getElementById('root')
   );
 }
